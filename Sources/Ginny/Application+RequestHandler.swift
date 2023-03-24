812a3481d2d788bcb02b72fd60a4d9753d73ddc7
Gonzalo Nuñez <gonzaloblasnunez@gmail.com>
2023-03-24 11:17:25 -0500

    Document using DoccGPT (#4)
    * Document Sources/Ginny/ using docc-gpt
    
    * Document Sources/GinnyCLI/ using docc-gpt
    
    * Add .spi.yml
    
    * Undo comment in generated code

--- Sources/Ginny/Application+RequestHandler.swift
+++ Sources/Ginny/Application+RequestHandler.swift
@@ -8,8 +8,16 @@
 import Foundation
 import Vapor
 
+/// Extension to `AsyncRequestHandler` to register a handler in an `Application`.
 extension AsyncRequestHandler {
 
+  /**
+   Registers a handler in an `Application` for a given path.
+
+   - Parameters:
+     - app: The `Application` to register the handler in.
+     - path: The path to register the handler for.
+   */
   public func register(in app: Application, for path: String) {
     app.on(method, path.pathComponents) { [handle] req async throws in
       try await handle(req)
@@ -17,11 +25,20 @@ extension AsyncRequestHandler {
   }
 }
 
+/// Extension to `RequestHandler` to register a handler in an `Application`.
 extension RequestHandler {
 
+  /**
+   Registers a handler in an `Application` for a given path.
+
+   - Parameters:
+     - app: The `Application` to register the handler in.
+     - path: The path to register the handler for.
+   */
   public func register(in app: Application, for path: String) {
     app.on(method, path.pathComponents) { [handle] req throws in
       try handle(req)
     }
   }
 }
+
