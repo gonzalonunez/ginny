92fc57876e95228ee0ca8e9079b99fb2fd8af0d3
Gonzalo Nuñez <gonzaloblasnunez@gmail.com>
2023-03-24 10:57:39 -0500

    Document Sources/Ginny/ using docc-gpt

--- Sources/Ginny/AsyncRequestHandler.swift
+++ Sources/Ginny/AsyncRequestHandler.swift
@@ -8,9 +8,24 @@
 import Foundation
 import Vapor
 
+/// A protocol for handling asynchronous requests.
 public protocol AsyncRequestHandler {
+  /// Creates a new instance of the handler.
   init()
+
+  /// The associated type of the response.
   associatedtype Response: AsyncResponseEncodable
+
+  /// The HTTP method of the request.
   var method: HTTPMethod { get }
+
+  /// Handles the request and returns a response.
+  ///
+  /// - Parameter req: The incoming request.
+  ///
+  /// - Throws: Any errors that occur while handling the request.
+  ///
+  /// - Returns: An `AsyncResponseEncodable` response.
   func handle(req: Request) async throws -> Response
 }
+
--- Sources/Ginny/RequestHandler.swift
+++ Sources/Ginny/RequestHandler.swift
@@ -8,9 +8,24 @@
 import Foundation
 import Vapor
 
+/// A protocol for handling requests.
 public protocol RequestHandler {
+  /// Initializes a new `RequestHandler`.
   init()
+
+  /// The associated type of the response.
   associatedtype Response: ResponseEncodable
+
+  /// The HTTP method of the request.
   var method: HTTPMethod { get }
+
+  /// Handles a request and returns a response.
+  ///
+  /// - Parameter req: The request to be handled.
+  ///
+  /// - Throws: Any errors that occur while handling the request.
+  ///
+  /// - Returns: The response to the request.
   func handle(req: Request) throws -> Response
 }
+
